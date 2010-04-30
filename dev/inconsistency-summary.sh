99794121779ceedee8a17537f881e3c992219d57
Francis M. Tyers <ftyers@prompsit.com>
2010-04-30 13:14:27 +0000

    more stuff

--- dev/inconsistency-summary.sh
+++ dev/inconsistency-summary.sh
@@ -30,9 +30,9 @@ for i in $POS; do
 		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v '<adj>'  | grep -v REGEX | wc -l`;
 		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v '<adj>' | grep -v REGEX |  wc -l`;
 	elif [ "$i" = "det" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v 'art<det>' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v 'art<det>'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v 'art<det>' | grep -v REGEX |  wc -l`;
+		TOTAL=`cat $INC | grep "<$i>" | grep -v 'art<' | grep -v REGEX | wc -l`; 
+		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v 'art<'  | grep -v REGEX | wc -l`;
+		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v 'art<' | grep -v REGEX |  wc -l`;
 	else
 		TOTAL=`cat $INC | grep "<$i>" | grep -v REGEX | wc -l`; 
 		AT=`cat $INC | grep "<$i>" | grep '@'  | grep -v REGEX | wc -l`;
